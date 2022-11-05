4f5ffcfef66717313bb5f41ae6148bd9b9141745
Mauricio Preto <mmprogrammer@hotmail.com>
2022-11-04 21:19:10 -0300

    H2 databe, test profile, JPA

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -8,13 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educandoweb.course.entities.User;
 
 @RestController
-@RequestMapping(value= "/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u= new User(1L,"Maria","maria@gmail.com","99999999","1245");
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
-	
 }
